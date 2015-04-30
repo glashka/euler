bbd8e92b315af382403bc311fdddf4890e6cdcac
glashka <d.migunova@gmail.com>
2015-04-30 15:44:49 +0200

    80 problem update 2

--- euler_project_problem_80.py
+++ euler_project_problem_80.py
@@ -3,11 +3,14 @@
 
 from decimal import *
 
+squares = set([1, 4, 9, 16, 25, 36, 49, 64, 81])
+
 getcontext().prec = 105
 
 result = 0
 
 for n in range(2, 100):
-    result += sum(int(i) for i in str(Decimal(n).sqrt()).replace('.', '')[:100])
+    if n not in squares:
+        result += sum(int(i) for i in str(Decimal(n).sqrt()).replace('.', '')[:100])
 
 print(result)
