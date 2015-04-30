60f60397341fc432f7a7565dcdd65e3fd11c7e23
glashka <d.migunova@gmail.com>
2015-04-30 15:39:06 +0200

    80 problem update

--- euler_project_problem_80.py
+++ euler_project_problem_80.py
@@ -3,12 +3,11 @@
 
 from decimal import *
 
-getcontext().prec = 100
+getcontext().prec = 105
 
 result = 0
 
 for n in range(2, 100):
-    result += sum(int(i) for i in str(Decimal(n).sqrt())[2:])
-
+    result += sum(int(i) for i in str(Decimal(n).sqrt()).replace('.', '')[:100])
 
 print(result)
